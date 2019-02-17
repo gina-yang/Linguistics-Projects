e97e87a93bd50c9dfbf99106140236a478264731
Gina Yang <ginayang@usc.edu>
2019-02-16 20:04:12 -0800

    Added vowelFinder function

--- PortmanteauCreator/PortmanteauScratch.py
+++ PortmanteauCreator/PortmanteauScratch.py
@@ -13,7 +13,6 @@ def commonEndLetter(string1, string2):
 # Sees if there is a string of length 3 in common between the words. If so, removes from the first word everything
 # after the common string and removes from the second word the common string and everything before it
 # and concatenates them
-
 # Returns "n/a" if no string in common. Otherwise, returns the concatenated string
 # Ex. hamster + termite = hamstermite
 def common3String(string1, string2):
@@ -23,6 +22,20 @@ def common3String(string1, string2):
                 return string1[:i] + string2[j+3:]
     return "n/a"
 
+# Returns a dictionary mapping vowel position in input string to vowel
+def vowelFinder(string):
+    vowels = ["a", "e", "i", "o", "u"]
+    counter = 0
+    vowelDict = {}
+    for i in string:
+        if i in vowels:
+            vowelDict[counter] = i
+        counter += 1
+    return vowelDict
+
+def vowelComp(vowelDict1, vowelDict2):
+
+
 # Portmanteau handler function
 def pme(string1, string2):
     return common3String(string1, string2)
@@ -31,8 +44,10 @@ def pme(string1, string2):
 def main():
     word1 = input("Enter a word: ")
     word2 = input("Enter another word: ")
-    portmanteau = pme(word1, word2)
-    print(portmanteau)
+
+    print(vowelFinder(word1))
+    print(vowelFinder(word2))
+    print(pme(word1, word2))
 
 
 main()
